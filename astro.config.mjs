befe4edbe6117b4f360cd96f5675a1f5ef9ab851
Will Kelly <willkelly@Will-Kellys-Pro.local>
2023-03-31 15:42:28 -0500

    removed iconify again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 import {defineConfig} from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import solidJs from "@astrojs/solid-js";
 import UnoCSS from "unocss/astro";
-import {presetUno} from "unocss";
+import presetUno from "@unocss/preset-uno";
 import transformerVariantGroup from "@unocss/transformer-variant-group";
 
 // https://astro.build/config
